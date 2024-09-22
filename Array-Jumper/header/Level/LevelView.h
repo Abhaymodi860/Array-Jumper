fd4cec9e5bc5bca98e8cb69d22dc7d4830a88807
Abhaymodi860 <modiabhay860@gmail.com>
2024-09-23 00:13:48 +0530

    level data added

--- Array-Jumper/header/Level/LevelView.h
+++ Array-Jumper/header/Level/LevelView.h
@@ -14,11 +14,28 @@ namespace Level
 		UI::UIElement::ImageView* background_image;
 		const float background_alpha = 110.f;
 
+		UI::UIElement::ImageView* box_image;
+		UI::UIElement::ImageView* target_overlay_image;
+		UI::UIElement::ImageView* letter_one_overlay_image;
+		UI::UIElement::ImageView* letter_two_overlay_image;
+		UI::UIElement::ImageView* letter_three_overlay_image;
+		UI::UIElement::ImageView* obstacle_one_overlay_image;
+		UI::UIElement::ImageView* obstacle_two_overlay_image;
+
+
+		BoxDimensions box_dimensions;
+
 		void createImages();
 		void initializeImages();
 		void updateImages();
 		void drawLevel();
+		void drawBox(sf::Vector2f position);
+		void drawBoxValue(sf::Vector2f position, BlockType box_value);
 		void deleteImages();
+
+		void calculateBoxDimensions();
+
+		UI::UIElement::ImageView* getBoxOverlayImage(BlockType block_type);
 
 	public:
 		LevelView(LevelController* controller);
